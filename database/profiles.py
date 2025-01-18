81282be0a78e29da09354d62d5d7ff0650fcb3b4
Kuineer <54183959+Kuineer@users.noreply.github.com>
2025-01-18 16:53:37 +0300

    database.db yeniden oluşturuldu.

--- database/profiles.py
+++ database/profiles.py
@@ -6,4 +6,4 @@ path.append("..")
 import utilities.generation as generation
 from connection import connection, cursor
 
-cursor.execute("CREATE TABLE IF NOT EXISTS profiles (username TEXT NOT NULL, expiration INTEGER NOT NULL, PRIMARY KEY (username))")
+cursor.execute("CREATE TABLE IF NOT EXISTS profiles (username TEXT NOT NULL, biography TEXT, has_picture INTEGER NOT NULL, PRIMARY KEY (username))")
--- utilities/generation.py
+++ utilities/generation.py
@@ -1,4 +1,4 @@
-import pyargon2
+#import pyargon2
 from datetime import datetime
 from uuid import UUID
 
@@ -17,7 +17,7 @@ def hashed_password(password):
     length = len(password)
     slice_index = length // 2 if length % 2 else length // 2 + 1
 
-    return pyargon2.hash(password[:slice_index], password[slice_index:])
+    return ""#pyargon2.hash(password[:slice_index], password[slice_index:])
 
 def message_id(time: datetime, user_id: UUID):
     return add_zeros(time.hour, 2) + add_zeros(time.minute, 2) + add_zeros(time.day, 2) + add_zeros(time.month, 2) + add_zeros(time.year, 4) + user_id.hex
