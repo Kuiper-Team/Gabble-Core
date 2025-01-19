29e3bb605477d4f4e2e54135c38a127ac8ca55b0
Kuineer <54183959+Kuineer@users.noreply.github.com>
2025-01-19 14:44:42 +0300

    Açıklama ve import eklendi.

--- daily_parallel_job.py
+++ daily_parallel_job.py
@@ -1,2 +1,5 @@
 #Linux Bash: python daily_parallel_job.py & python launch.py && fg
 #Her gün saat 00:00da timed_keys tablosunu denetle ve tarihi geçmiş olanları sil.
+from database.connection import connection, cursor
+
+#Saat 00:00ı bekleyen sistem...
