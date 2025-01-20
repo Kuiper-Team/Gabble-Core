407602a0cc47d3b38e374cd4c7eab8d6d4e2f5a2
Kuineer <54183959+Kuineer@users.noreply.github.com>
2025-01-20 16:37:31 +0300

    profiles.py düzenlendi ve launch.py'ye açıklama eklendi.

--- database/profiles.py
+++ database/profiles.py
@@ -1,8 +1,17 @@
+import sqlite3
 from sys import path
 
 path.append("..")
 
-import utilities.generation as generation
 from connection import connection, cursor
 
 cursor.execute("CREATE TABLE IF NOT EXISTS profiles (username TEXT NOT NULL, biography TEXT, has_picture INTEGER NOT NULL, PRIMARY KEY (username))")
+
+def update_profile(username, biography, has_picture): #"username" değiştirilemez. (Belgelerde gerekçe yazacağım.)
+    def update_settings(username, settings, hash):
+        try:
+            cursor.execute("UPDATE profiles SET biography = ?, has_picture = ? WHERE username = ?", (biography, 0 if has_picture == False else 1, username))
+        except sqlite3.OperationalError:
+            raise Exception("couldntinsert")
+        else:
+            connection.commit()
--- launch.py
+++ launch.py
@@ -1,1 +1,2 @@
 #https://docs.python.org/3/library/multiprocessing.html
+#database.connection modülü çağrılıp uygun koşullarda connection.close() çalıştırılacak.
