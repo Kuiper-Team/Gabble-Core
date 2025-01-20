0bd5e741d043dc2893bb0a15427eef5879302488
Kuineer <54183959+Kuineer@users.noreply.github.com>
2025-01-20 16:10:07 +0300

    users.py'de yenilikler yapıldı.

--- database/users.py
+++ database/users.py
@@ -13,10 +13,11 @@ from connection import connection, cursor
 cursor.execute("CREATE TABLE IF NOT EXISTS users (username TEXT NOT NULL, toc INTEGER NOT NULL, hash TEXT NOT NULL, settings TEXT NOT NULL, friends TEXT, PRIMARY KEY (username))") #"friends" ve "settings", "hash" anahtarı kullanılarak AES ile oluşturulan bir şifredir.
 #"hash" ve "settings" birer hash ve "settings", "hash" kullanılarak oluşturulmuş bir hashtır.
 
-def create_user(username, password): #profile.db de katılacak.
+def create_user(username, password):
     hash = generation.hashed_password(password)
     try:
         cursor.execute("INSERT INTO users VALUES (?, ?, ?, ?, ?)", (username, generation.unix_timestamp(datetime.now()), hash, sha_aes_encrypt(database.default_settings, hash[:16], hash[16:]), None))
+        cursor.execute("INSERT INTO profiles VALUES (?, ?, ?)", (username, None, 0))
     except sqlite3.OperationalError:
         raise Exception("userexists")
     else:
@@ -31,7 +32,6 @@ def delete_user(username):
         connection.commit()
 
 def add_friends(username_1, username_2, hash_1, hash_2):
-    encrypted_1, encrypted_2 = (None,) * 2
     try:
         encrypted_1 = cursor.execute("SELECT friends FROM users WHERE username = ?", (username_1,)).fetchone()[0]
         encrypted_2 = cursor.execute("SELECT friends FROM users WHERE username = ?", (username_2,)).fetchone()[0]
@@ -40,16 +40,21 @@ def add_friends(username_1, username_2, hash_1, hash_2):
         friends_2 = generation.sha_aes_decrypt(encrypted_2, hash_2[16:], hash_2[:16])
     except sqlite3.OperationalError:
         raise Exception("nouser")
+    else:
+        try:
+            new_encrypted_1 = generation.sha_aes_encrypt(friends_1 + "," + username_2, hash_1[16:], hash_1[:16])
+            new_encrypted_2 = generation.sha_aes_encrypt(friends_2 + "," + username_1, hash_2[16:], hash_2[:16])
+            cursor.execute("INSERT OR REPLACE INTO users (friends) VALUES(?) WHERE username = ?", (new_encrypted_1, username_1))
+            cursor.execute("INSERT OR REPLACE INTO users (friends) VALUES(?) WHERE username = ?", (new_encrypted_2, username_2))
+        except sqlite3.OperationalError:
+            raise Exception("couldntinsert")
+        else:
+            connection.commit()
 
+def update_settings(username, settings, hash):
     try:
-        new_encrypted_1 = generation.sha_aes_encrypt(friends_1 + "," + username_2, hash_1[16:], hash_1[:16])
-        new_encrypted_2 = generation.sha_aes_encrypt(friends_2 + "," + username_1, hash_2[16:], hash_2[:16])
-        cursor.execute("INSERT INTO users (friends) VALUES(?) WHERE username = ?", (new_encrypted_1, username_1))
-        cursor.execute("INSERT INTO users (friends) VALUES(?) WHERE username = ?", (new_encrypted_2, username_2))
+        cursor.execute("UPDATE users SET settings = ? WHERE username = ?", (generation.sha_aes_encrypt(settings, hash[16:], hash[:16]), username))
     except sqlite3.OperationalError:
         raise Exception("couldntinsert")
     else:
         connection.commit()
-
-def apply_settings(username, settings):
-    pass #EĞER KULLANICI VARSA settings sütununu düzenleyecek.
