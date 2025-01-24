d1de030d42fcda873c05171dde008ea256effcb8
Kuineer <54183959+Kuineer@users.noreply.github.com>
2025-01-24 19:09:58 +0300

    Açıklama eklendi.

--- launch.py
+++ launch.py
@@ -1,2 +1,3 @@
 #https://docs.python.org/3/library/multiprocessing.html
 #database.connection modülü çağrılıp uygun koşullarda connection.close() çalıştırılacak.
+#KESİNLİKLE threading kullanılmalı.
